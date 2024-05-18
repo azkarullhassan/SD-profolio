5de16068529c11857a65cf7bdf660ae37a788477
azkarullhassan <azkarullhassan7@gmail.com>
2024-05-18 22:16:08 +0500

    first commit

--- app/components/Nine.jsx
+++ app/components/Nine.jsx
@@ -7,8 +7,8 @@ const Nine = () => {
            <div className='flex justify-center '>
             <img src="/images/image 117.png" alt="arr" />
            </div>
-           <h1 className='font-medium text-[56px] text-center'>Got a project? Let’s talk</h1>
-           <h2 className='font-normal text-[18px] text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In urna, non <br /> nisl tincidunt ut elementum turpis.</h2>
+           <h1 className='font-medium text-[56px] text-center'>Got a sjdkkd? Let’s talk</h1>
+           <h2 className='font-normal text-[18px] text-center'>Lorem smskkkm dolor sit amet, consectetur adipiscing elit. In urna, non <br /> nisl tincidunt ut elementum turpis.</h2>
           <div className='flex justify-center pt-8'>
           <button className='border-[1px] md:px-5 px-3 py-3 bg-[#5D3BEE]  text-[16px] text-white font-bold'>
            Book a Call
