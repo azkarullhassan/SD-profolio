b39211e10709ac5e40ea4390ddb7f347b5911794
azkarullhassan <azkarullhassan7@gmail.com>
2024-06-24 22:33:47 +0500

    first commit

--- app/components/Seven.jsx
+++ app/components/Seven.jsx
@@ -14,7 +14,7 @@ const Seven = () => {
        <div className=' h-0.5 border-2 border-[#a2a0ad] my-6'></div>
         
        <div className='flex justify-between pt-5 '>
-           <h1 className='font-medium md:text-[32px] text-[18px]  '>What  of ojfeh3i do yu take on?</h1>
+           <h1 className='font-medium md:text-[32px] text-[18px]  '>Lorem ipsum dolor sit amet.</h1>
            <ion-icon name="add-circle-outline" size="large"></ion-icon>
        </div>
        <div className=' h-0.5 border-2 border-[#000000] my-6'></div>
